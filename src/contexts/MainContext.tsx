dcb9cf4ac82ccdbb172034b1df9bfafc7c55806b
Marcelo Bracet <marcelo.bracet1@gmail.com>
2024-03-29 10:00:17 -0300

    Add new font files and update global styles

---
+++ src/contexts/MainContext.tsx
@@ -0,0 +1,15 @@
+import { createContext, useContext } from "react";
+
+interface MainContextType {}
+
+const AppContext = createContext<MainContextType | undefined>({});
+
+export const AppProvider = ({ children }: { children: React.ReactNode }) => {
+  const value = {};
+
+  return <AppContext.Provider value={value}>{children}</AppContext.Provider>;
+};
+
+export const useMainContext = () => {
+  return useContext(AppContext);
+};
