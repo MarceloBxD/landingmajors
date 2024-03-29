62fc34a4aaf238e39b5185007c889f251d957108
Marcelo Bracet <marcelo.bracet1@gmail.com>
2024-03-29 10:03:15 -0300

    Update import path for global styles and add client-side code

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import "@/app/globals.scss";
+import "@/app/styles/globals.scss";
 import { AppProvider } from "@/contexts/MainContext";
 
 const inter = Inter({ subsets: ["latin"] });
